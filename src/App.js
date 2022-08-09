cd65d2af53631b2776c2ab586efcd295f8af6379
Maria Redman <absmcc@gmail.com>
2022-08-09 15:35:06 -0400

    try-catch

--- src/App.js
+++ src/App.js
@@ -3,15 +3,47 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formSubmitted, setFormSubmitted] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("")
   const [title, setTitle] = useState ("this is the title")
   const [description, setDescription] = useState("Your description")
-  const [author, setAuthor] = useState("Todd albert")
+  const [author, setAuthor] = useState("Todd Albert")
   console.log(title)
-function formSubmit(e) {
+async function formSubmit(e) {
+  try{
+
   e.preventDefault();
-  console.log("form submitted")
+  //console.log("form submitted")
+
+// const comment = {
+//   title:title,
+//   description:description,
+//   author:author,
+// }
+// console.log("formSubmitted", comment)
+const comment = {
+  title,
+  description,
+  author,
 }
 
+
+const results =  await fetch("https://sql.bocacode.com/comments",{
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(comment)
+})
+const data = await results.json()
+
+
+//console.log(data)
+
+} catch (error){
+console.error(error)
+}
+}
 
   return (
     <div className="App">
@@ -37,7 +69,7 @@ function formSubmit(e) {
       onChange={(e)=>{setAuthor(e.target.ariaValueMin)}}
 >
   <option value ="">Choose One</option>
-  <option value ="Todd albert">Todd</option>
+  <option value ="Todd Albert">Todd</option>
   <option value ="ludwigson">Ludwigson</option>
   <option value ="other">Other</option>
 
